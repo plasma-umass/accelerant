516ae5bdddf9b1dc19b542c89365c2ea4ad40467
Noah Lev <camelidcamel@gmail.com>
2025-03-03 16:33:10 -0500

    Automatically find hotspots using perf data

---
+++ optastic/perf.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from typing import Callable, Iterator, List, Optional
+
+from optastic.project import Project
+
+
+class PerfData:
+    counts: dict[tuple[Path, int], int]
+
+    def __init__(self, perf_out_path: Path, project: Project):
+        def is_srcline_good(path: Path):
+            return project._root in path.parents
+
+        counts = {}
+        with open(perf_out_path) as f:
+            while True:
+                try:
+                    result = extract_srcline_from_perf_entry(iter(f), is_srcline_good)
+                    if not result:
+                        continue
+                    sym, lineno = result
+                except StopIteration:
+                    break
+                if (sym, lineno) not in counts:
+                    counts[(sym, lineno)] = 0
+                counts[(sym, lineno)] += 1
+        self.counts = counts
+
+    def normalize_and_sort(self) -> List[tuple[tuple[Path, int], float]]:
+        tot_samples = sum(self.counts.values())
+        counts_srt = list(
+            map(
+                lambda kv: (kv[0], kv[1] / tot_samples),
+                sorted(self.counts.items(), key=lambda kv: kv[1], reverse=True),
+            )
+        )
+        return counts_srt
+
+
+def extract_srcline_from_perf_entry(
+    lines: Iterator[str], is_srcline_good: Callable[[Path], bool]
+) -> Optional[tuple[Path, int]]:
+    """
+    Parses a `perf script` entry from the provided iterator,
+    and returns the first "good" line location from the stack trace,
+    based on the provided callback.
+
+    Meant to be run on output from `perf script -F+srcline --full-source-path`.
+    """
+
+    _ = next(lines)  # skip header
+    while True:
+        line = next(lines).strip()  # ip/sym
+        if not line:
+            break
+        line = next(lines).strip()  # srcline
+        if not line:
+            break
+        loc, *info = line.split(" ", 1)
+        if ":" not in loc:
+            continue
+        split_results = loc.rsplit(":", 1)
+        path, lineno = Path(split_results[0]), int(split_results[1])
+        if is_srcline_good(path):
+            return path, lineno
+
+    return None
--- optastic_server.py
+++ optastic_server.py
@@ -1,28 +1,48 @@
+import os
 from pathlib import Path
+from typing import Optional
 from flask import Flask, request
 
 from optastic.chat import run_chat
+from optastic.perf import PerfData
 from optastic.project import Project
 
 app = Flask(__name__)
 
 
 @app.route("/optimize")
-def route_optimize():
+def route_optimize() -> str:
     project = request.args.get("project", type=Path)
     if project is None:
         raise Exception("invalid project path")
     filename = request.args.get("filename")
     lineno = request.args.get("line", type=int)
-    if lineno is None:
-        raise Exception("invalid line number")
+    perf_out_path = request.args.get("perfOutPath", type=Path)
     model_id = request.args.get("modelId", "o3-mini")
 
-    response = optimize(project, filename, lineno, model_id)
+    if (filename is None or lineno is None) and perf_out_path is None:
+        raise Exception(
+            "at least one of (filename and line) or (perfOutPath) must be passed"
+        )
+
+    response = optimize(project, filename, lineno, perf_out_path, model_id)
     return response
 
 
-def optimize(project_root: Path, filename: str, lineno: int, model_id: str) -> str:
+def optimize(
+    project_root: Path,
+    filename: Optional[str],
+    lineno: Optional[int],
+    perf_out_path: Optional[Path],
+    model_id: str,
+) -> str:
     project = Project(project_root, "rust")
+    perf_data = None
+    if perf_out_path:
+        perf_data = PerfData(perf_out_path, project)
+    if filename is None or lineno is None:
+        assert perf_data is not None
+        abspath, lineno = perf_data.normalize_and_sort()[0][0]
+        filename = os.path.relpath(abspath, project._root)
     with project.lsp().start_server():
         return run_chat(project, filename, lineno, model_id)
